3d06e45dd7e99a82f4018370f7b76645977898d9
jmbaik <famian@daum.net>
2023-05-09 02:14:39 +0900

    admin user controller 수정

--- src/main/java/metel/salem/api/user/controller/AdminUserController.java
+++ src/main/java/metel/salem/api/user/controller/AdminUserController.java
@@ -18,6 +18,9 @@ public class AdminUserController {
     @PostMapping("/login")
     public ResponseDto<AdminUserDto> getAdminLoginInfo(@RequestBody AdminUserDto adminUserDto){
         AdminUserDto info = service.selectAdminLoginInfo(adminUserDto);
+        if(info == null){
+            return ApiResponse.Error("해당 사용자가 없습니다. 다시 입력하여 주십시요", null);
+        }
         return ApiResponse.Success(info);
     }
 
